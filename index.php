a2315b443044a9f1776fc6396457848618b71324
Camille Granade <camgranade@gmail.com>
2020-08-09 19:00:25 +0200

    Changed "libre" to "disponible" Edited css to more visible submit button Done: first/ last name Email/phone number tried, not working ;

--- index.php
+++ index.php
@@ -4,17 +4,17 @@
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>Cimade - Francais</title>
+      <title>Cimade - Français</title>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
       <link rel="stylesheet" type="text/css" href="style.css">
     </head>
 
   <body>
     <h1 class="text">Cimade</h1>
-    <h2>Cours de Francais</h2>
+    <h2>Cours de Français</h2>
     <?php
      $hours = array();
-     if (($handle = fopen("/tmp/file.csv", "r")) !== FALSE) {
+     if (($handle = fopen("C:/wamp64/tmp/file.csv", "r")) !== FALSE) {
        while (($data = fgetcsv($handle)) !== FALSE) {
          $hours[] = $data[4];
        }
@@ -31,28 +31,35 @@
      function display_td($hour, $display) {
        $complet = complet($hour);
        echo '          <td class="' . ($complet ? 'complet' : 'libre') . '">'."\n";
-       echo '          <input type="radio" name="horaire" id="' . $hour . '" value="' . $hour . '"' . ($complet ? ' disabled' : '') . '>'."\n";
+       echo '          <input type="radio" name="horaire" id="' . $hour . '" value="' . $hour . '"' . ($complet ? ' disabled' : '') . 'checked>'."\n";
        echo '          <label for="' . $hour . '"' . ($complet ? ' disabled' : '') . '>' . $display . '</label>'."\n";
-       echo '          ' . ($complet ? '<p>Complet</p>' : '<p>Libre</p>')."\n";
+       echo '          ' . ($complet ? '<p>Complet</p>' : '<p>Disponible</p>')."\n";
        echo '          </td>'."\n";
      }
 
-     $prenom = isset($_POST['prenom']) ? $_POST['prenom'] : NULL;
-     $nom = isset($_POST['nom']) ? $_POST['nom'] : NULL;
-     $telephone = isset($_POST['telephone']) ? $_POST['telephone'] : NULL;
-     $email = isset($_POST['email']) ? $_POST['email'] : NULL;
-     $horaire = isset($_POST['horaire']) ? $_POST['horaire'] : NULL;
+     $prenom = isset($_POST['prenom']) ? $_POST['prenom'] : "";
+     $nom = isset($_POST['nom']) ? $_POST['nom'] : "";
+     $telephone = isset($_POST['telephone']) ? $_POST['telephone'] : "";
+     $email = isset($_POST['email']) ? $_POST['email'] : "";
+     $horaire = isset($_POST['horaire']) ? $_POST['horaire'] : "";
      $ok = true;
-    
-     echo "<div style='error'>\n";
+
+
      if ($_SERVER["REQUEST_METHOD"] == "POST") {
-       if (empty($prenom) || empty($nom)) {
-          echo "<p>Entrez votre nom et prenom.</p>\n";
+       echo "<div class='error'>\n";
+       if (empty($prenom) || empty($nom) || (strlen($prenom) < 2) || (strlen($nom) < 2)) {
+          echo "<p>Entrez votre nom et prénom.</p>\n";
           $ok = false;
        }
+
+       $num = preg_replace("/[^0-9]/", "", "$telephone");
+       if ((strlen($num) < 6) || (!filter_var($email, FILTER_VALIDATE_EMAIL)) ) {
+          echo "<p>Veuillez rentrer un numéro de téléphone ou un email valide.</p>\n";
+          $ok = false;
+        }
        // TODO: more validation
-       // - first name and last name longer than 2 characters
-       // - either email or phone number
+       // DONE: first name and last name longer than 2 characters
+       // TODO either email or phone number
        // - if email present, email format is correct
        // - if phone number present, phone number is correct
        // - maybe more :)
@@ -61,17 +68,20 @@
           echo "<p>Le creneau choisi est complet, choisissez un autre creneau.</p>\n";
           $ok = false;
        }
+
        echo "</div>\n";
-  
+
        if ($ok) {
-         $fp = fopen('/tmp/file.csv', 'aw');
+         $fp = fopen('C:wamp64/tmp/file.csv', 'aw');
          fputcsv($fp, array($prenom, $nom, $telephone, $email, $horaire));
          fclose($fp);
-  
-         echo "votre rendez vous a ete pris pour : ";
-         // TODO display summary 
+
+         echo "Votre rendez vous a ete pris pour " . $horaire . " ";
+         // TODO display summary
        }
+
      } // ... "POST"
+
 
      if (!($_SERVER["REQUEST_METHOD"] == "POST") || !($ok)) {
      ?>
@@ -79,17 +89,18 @@
     <p>Rendez-vous d'inscription:</p>
 
     <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
-      <label style="margin-left:1em" for="prenom">Prenom:</label>
+      <label style="margin-left:1em" for="prenom">Prénom:</label>
       <input type="text" name="prenom" id="prenom" value="<?php echo htmlentities($prenom) ?>">
       <label style="margin-left:1em" for="nom">Nom:</label>
       <input type="text" name="nom" id="nom" value="<?php echo htmlentities($nom) ?>"><br>
-      <label style="" for="telephone">Telephone:</label>
+      <label style="" for="telephone">Téléphone:</label>
       <input type="text" name="telephone" id="telephone" value="<?php echo htmlentities($telephone) ?>">
-      <p style="display:inline; margin-left: 1em;">et/out</p>
+      <p style="display:inline; margin-left: 1em;">et/ou</p>
       <label style="margin-left:1em" for="email">Email:</label>
       <input type="text" name="email" id="email" value="<?php echo htmlentities($email) ?>">
       <br/>
       <table class="horaire" border="3" cellspacing="4" align="left">
+        <caption><input class="submit" style="" type="submit" value="Finir"></caption>
         <tr>
           <th>Mercredi</th>
           <th>Jeudi</th>
@@ -144,8 +155,8 @@
            display_td("sam17h30", "17h30");
            ?>
         </tr>
-      </table> <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-      <input style="" type="submit" value="Finir">
+      </table>
+
     </form>
     <?php  } ?>
   </body>
