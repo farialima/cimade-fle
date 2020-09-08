5617d85c7599f9d7e27fb574c058c8301a1ebbbb
Francois Granade <github.com@farialima.net>
2020-09-08 15:59:12 +0200

    cosmetic

--- upload.sh
+++ upload.sh
@@ -2,13 +2,12 @@ sftp  -P 21098 fariqxys@ftp.farialima.net:/home/fariqxys/cimade.farialima.net/ <
 put index.html
 put functions.php
 put style.css
-mkdir fle
+#mkdir fle
 put fle/index.php fle/index.php
-mkdir fle/resultats
-put -r fle/resultats/index.html fle/resultats/index.html
-mkdir benevoles
+#mkdir fle/resultats
+put fle/resultats/index.html fle/resultats/index.html
+#mkdir benevoles
 put benevoles/index.php benevoles/index.php
-mkdir benevoles/resultats
-put -r benevoles/resultats/index.html benevoles/resultats/index.html
-
+#mkdir benevoles/resultats
+put benevoles/resultats/index.html benevoles/resultats/index.html
 EOF
