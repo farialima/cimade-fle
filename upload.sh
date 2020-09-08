372fdec05ca53cea980c06127546b49087ffc5f4
Francois Granade <github.com@farialima.net>
2020-09-08 15:56:56 +0200

    index file, upload file

---
+++ upload.sh
@@ -0,0 +1,14 @@
+sftp  -P 21098 fariqxys@ftp.farialima.net:/home/fariqxys/cimade.farialima.net/ <<EOF
+put index.html
+put functions.php
+put style.css
+mkdir fle
+put fle/index.php fle/index.php
+mkdir fle/resultats
+put -r fle/resultats/index.html fle/resultats/index.html
+mkdir benevoles
+put benevoles/index.php benevoles/index.php
+mkdir benevoles/resultats
+put -r benevoles/resultats/index.html benevoles/resultats/index.html
+
+EOF
