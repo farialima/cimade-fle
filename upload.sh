3d10053a3bef85f02778084733ff7bd0acdf8ca5
Francois Granade <github.com@farialima.net>
2020-09-11 14:36:02 +0200

    simplier upload file

--- upload.sh
+++ upload.sh
@@ -6,16 +6,10 @@ put resultat.html
 
 #mkdir fle
 put fle/index.php fle/index.php
-rm fle/resultats/index.php
-rmdir fle/resultats
-put fle/resultats fle/resultats
 put fle/resultats.php fle/resultats.php
 
 #mkdir benevoles
 put benevoles/index.php benevoles/index.php
-rm benevoles/resultats/index.php
-rmdir benevoles/resultats
-put benevoles/resultats benevoles/resultats
 put benevoles/resultats.php benevoles/resultats.php
 
 #also getting the latest results...
